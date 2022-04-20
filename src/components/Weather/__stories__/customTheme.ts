f88de4f4a9e013b7fe6ba7ce12cf1bc1d77b52e2
Mark Rabey <mark@markrabey.com>
2022-04-20 13:08:05 -0400

    update custom theme stories

--- src/components/Weather/__stories__/customTheme.ts
+++ src/components/Weather/__stories__/customTheme.ts
@@ -1,23 +1,8 @@
 import {Theme} from '../../../themes/types';
-
+import lightTheme from '../../../themes/light';
 const customTheme: Theme = {
+  ...lightTheme,
   name: 'custom-theme',
-  containerStyles: {
-    backgroundColor: 'red',
-    borderColor: 'red',
-  },
-  headingStyles: {
-    fontFamily: 'serif',
-    color: '#efefef',
-  },
-  subheadingStyles: {
-    fontFamily: 'serif',
-    color: '#efefef',
-  },
-  detailStyles: {
-    fontFamily: 'serif',
-    color: '#efefef',
-  },
 };
 
 export default customTheme;
